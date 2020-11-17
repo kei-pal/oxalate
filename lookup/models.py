b4f683786bc483aa75459f96a0472c4e1cc3cdca
Kei Palahniuk <keipalahniuk@gmail.com>
2020-11-17 17:44:07 +0200

    added Ingredient model and migrations

--- lookup/models.py
+++ lookup/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+	name = models.CharField(max_length=50)
+	portion_desc = models.CharField(max_length=50)
+	portion_grams = models.IntegerField()
+	alternatives = models.CharField(max_length=200)
+	ox_per_100g = models.DecimalField(max_digits=5, decimal_places=2)
+	source_notes = models.TextField()
