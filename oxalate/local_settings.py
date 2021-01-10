cf1de520fea304ad4d74c02ecd3ef80ce5b33a10
Kei <keipalahniuk@gmail.com>
2021-01-10 21:54:24 +0000

    .gitignore db and local settings

--- oxalate/local_settings.py
+++
@@ -1,4 +0,0 @@
-DEBUG = True
-SECRET_KEY = '2k9^_1mc##-oy#3jaezea*rwl$!u6*l5viqy)a9gbe7#^5x=^n'
-ALLOWED_HOSTS = ['keipalahniuk.pythonanywhere.com']
-STATIC_ROOT = '/home/keipalahniuk/oxalate/static'
